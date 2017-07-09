9cd1b96aacea4f006b0bfb212b7a4babc32cfb02
zqy628 <191155395@qq.com>
2017-07-09 19:22:22 +0800

    pygame

--- game.py
+++ game.py
@@ -5,15 +5,20 @@ import pygame
 from sys import exit
 import random
 pygame.init() #初始化
-screen = pygame.display.set_mode((600,300),pygame.RESIZABLE,0)  #分辨率参数是一对表示宽度和高度的数字。flags参数是其他选项的集合。深度参数表示用于颜色的位数。
-pygame.display.set_caption('Hello,world!')  #设置窗口标题
-backgroud1 = pygame.image.load('0.jpg').convert()  #加载并转换图像
-backgroud2 = pygame.image.load('1.jpg').convert()  # 加载并转换图像
-
+screen = pygame.display.set_mode((540,960))  #分辨率参数是一对表示宽度和高度的数字。flags参数是其他选项的集合。深度参数表示用于颜色的位数。
+pygame.display.set_caption('Thunder!')  #设置窗口标题
+backgroud = pygame.image.load('backgroud.png').convert()  #加载并转换图像
+plane = pygame.image.load('plane.png').convert_alpha()  # 加载并转换图像
+bullet = pygame.image.load('bullet.png').convert_alpha()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            screen.blit(random.choice([backgroud1,backgroud2]),(0,0))  #将背景图画上去
-        pygame.display.update()   #刷新一下画面
+            exit()
+    screen.blit(backgroud,(0,0))
+    x,y = pygame.mouse.get_pos()
+    x -= plane.get_width()/2
+    y -= plane.get_height()/2
+    screen.blit(plane,(x,y))
+    # screen.blit(backgroud, (0, 0))
+    pygame.display.flip()   #刷新一下画面
